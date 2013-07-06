75133073072e27c8f93ebf6826ae9364ccb1300e
Mark Bate <mark.bate@mac.com>
2013-07-06 16:41:23 +0200

    Create a new cookbook

---
+++ README.md
@@ -0,0 +1,13 @@
+# sharepoint cookbook
+
+# Requirements
+
+# Usage
+
+# Attributes
+
+# Recipes
+
+# Author
+
+Author:: YOUR_NAME (<YOUR_EMAIL>)
---
+++ metadata.rb
@@ -0,0 +1,10 @@
+name             'sharepoint'
+maintainer       'Mark Bate'
+maintainer_email 'mark.bate@mac.com'
+license          'Apache 2.0'
+description      'Installs/Configures Microsoft Sharepoint'
+long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
+version          '0.1.0'
+
+depends          'windows'
+depends          'powershell'
