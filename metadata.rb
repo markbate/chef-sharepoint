86b7d6885017d23616a58bf5c3c45395b1ae858e
Mark Bate <mark.bate@mac.com>
2013-07-08 16:18:58 +0200

    version bump

--- metadata.rb
+++ metadata.rb
@@ -4,7 +4,7 @@ maintainer_email 'mark.bate@mac.com'
 license          'Apache 2.0'
 description      'Installs/Configures Microsoft Sharepoint'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '0.1.1'
+version          '0.1.2'
 
 depends          'windows'
 depends          'powershell'
