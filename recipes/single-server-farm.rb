15715d24d9e5bed55fc339dd97dcb58b2af3d577
Mark Bate <mark.bate@mac.com>
2013-07-08 14:28:24 +0200

    cleaned up to test installing from F:\

--- recipes/single-server-farm.rb
+++ recipes/single-server-farm.rb
@@ -1,27 +1,21 @@
-DOWNLOAD_URL = "http://care.dlservice.microsoft.com//dl/download/3/D/7/3D713F30-C316-49B8-9CC0-E1BFC34B63A0/SharePointServer_x64_en-us.img"
+download_url = "http://care.dlservice.microsoft.com//dl/download/3/D/7/3D713F30-C316-49B8-9CC0-E1BFC34B63A0/SharePointServer_x64_en-us.img"
 
 node.override['sharepoint']['server_role'] = "SINGLESERVER"
 
-remote_file "sharepoint image" do
-  # map this to the DOWNLOAD_URL
-end
 
-
-
-
-
-template "c:\temp\sharepoint-config.xml" do
+template "C:\\temp\\sharepoint-config.xml" do
   source "config.xml.erb"
   rights :read, "Everyone"
 end
 
 windows_package "sharepoint preparation" do
-  
+  source "F:\\prerequisiteinstaller.exe"
   action :install
+  options "/unattended"
 end
 
 windows_package "sharepoint" do
-  source DOWNLOAD_URL
+  source "F:\\setup.exe"
   action :install
-  options "/config c:\temp\sharepoint-config.xml"
+  options "/config C:\\temp\\sharepoint-config.xml"
 end
