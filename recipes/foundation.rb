34d5748cff7999c6b4ff2358f7d89c57f03b75bd
Mark Bate <mark.bate@mac.com>
2013-07-09 14:16:08 +0200

    set the install type for the template

--- recipes/foundation.rb
+++ recipes/foundation.rb
@@ -1,5 +1,5 @@
 download_url = "http://download.microsoft.com/download/3/E/8/3E85BC6B-6024-459E-8415-0ED8A21DD11A/sharepoint.exe"
-
+node.override['sharepoint']['server_role'] = "APPLICATION"
 
 # create the config file
 template "C:/Windows/Temp/sharepoint-config.xml" do
