e21aa6bbe092f1adaa164ae6de1f0af5e346af40
Mark Bate <mark.bate@mac.com>
2013-07-09 14:12:47 +0200

    hopefully this will drive silent install for foundation

--- recipes/foundation.rb
+++ recipes/foundation.rb
@@ -1,6 +1,15 @@
-DOWNLOAD_URL = "http://download.microsoft.com/download/3/E/8/3E85BC6B-6024-459E-8415-0ED8A21DD11A/sharepoint.exe"
+download_url = "http://download.microsoft.com/download/3/E/8/3E85BC6B-6024-459E-8415-0ED8A21DD11A/sharepoint.exe"
 
+
+# create the config file
+template "C:/Windows/Temp/sharepoint-config.xml" do
+  source "config.xml.erb"
+  rights :read, "Everyone"
+end
+
+# install sharepoint
 windows_package "sharepoint" do
-  source DOWNLOAD_URL
+  source download_url
   action :install
+  options "/config C:/Windows/Temp/sharepoint-config.xml"
 end
