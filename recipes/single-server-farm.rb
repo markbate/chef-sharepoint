07d8bf4f93fd3a9b3bb9a3085f0eacce57bc2435
Mark Bate <mark.bate@mac.com>
2013-07-09 14:23:02 +0200

    lets try using an env var & see what happens

--- recipes/single-server-farm.rb
+++ recipes/single-server-farm.rb
@@ -6,15 +6,15 @@ remote_file download_to do
   source download_url
 end
 
-# mount the image as a drive & get the letter back
-# windows doesn't let us set the mount point :/
-node.default['sharepoint']['mount_pount'] = powershell "mount SPS image" do
+# mount the image as a drive. Powershell doesn't appear to let us define a
+# drive letter, so I'm throwing it back to chef via an env var. super hacky!
+powershell "mount SPS image" do
   code <<-EOH
   $mountResult = Mount-DiskImage -PassThru -ImagePath "#{download_to}"
-  ($mountResult | Get-Volume).DriveLetter
+  $driveLetter = ($mountResult | Get-Volume).DriveLetter
+  [Environment]::SetEnvironmentVariable("SPS_Mount", $driveLetter, "User")
   EOH
 end
-Chef::Log.info("Mounted the image as drive: #{node.default['sharepoint']['mount_pount']}")
 
 # create the config file
 template "C:/Windows/Temp/sharepoint-config.xml" do
@@ -25,14 +25,14 @@ end
 # run the prereq installer. not sure, but this might need to restart things.
 # if it does, then obviously it'll break the connection. silly windows.
 windows_package "sharepoint preparation" do
-  source "#{node.default['sharepoint']['mount_pount']}:/prerequisiteinstaller.exe"
+  source "#{ENV['SPS_Mount']}:/prerequisiteinstaller.exe"
   action :install
   options "/unattended"
 end
 
 # install sharepoint
 windows_package "sharepoint" do
-  source "#{node.default['sharepoint']['mount_pount']}:/setup.exe"
+  source "#{ENV['SPS_Mount']}:/setup.exe"
   action :install
   options "/config C:/Windows/Temp/sharepoint-config.xml"
 end
